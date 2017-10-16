b7e8c78c91a997f7cb3ffe2e607c2771efb97275
Chris <chrisdavidramos@gmail.com>
2017-10-15 21:53:56 -0400

    making progress on test server

--- app/server/router.php
+++ app/server/router.php
@@ -1,9 +1,13 @@
 <?php
 // Set timezone
 date_default_timezone_set("UTC");
-echo dirname(__FILE__, 2);die();
+define("VENDOR", dirname(__FILE__, 2)."/vendor");
+require_once dirname(__FILE__, 2) . "/vendor/autoload.php";
 // Directory that contains error pages
 define("ERRORS", dirname(__FILE__) . "/views/errors");
+define("VIEWS", dirname(__FILE__) . "/views");
+
+
 
 // Default index file
 define("DIRECTORY_INDEX", "index.php");
@@ -34,14 +38,20 @@ if (empty($ext)) {
     $path = rtrim($path, "/") . "/" . DIRECTORY_INDEX;
 }
 
-// If the file exists then return false and let the server handle it
+//If the file exists then return false and let the server handle it
 if (file_exists($_SERVER["DOCUMENT_ROOT"] . $path)) {
     return false;
 }
 
 
+$resource = VIEWS.$_SERVER["REQUEST_URI"];
+if (file_exists($resource)) {
+    require_once $resource;
+    return true;
+}
+
 
 // default behavior
-logAccess(404);
+    logAccess(404);
 http_response_code(404);
 include ERRORS . "/404.php";
---
+++ test.php
@@ -0,0 +1,3 @@
+<?PHP
+
+$doc = new DOMDocum
